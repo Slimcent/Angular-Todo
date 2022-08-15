cce67abaa532ccc9205b0a9aaf8b6659f4cbca7d
Slimcent <33299753+Slimcent@users.noreply.github.com>
2022-08-15 23:55:22 +0100

    Add, delete and update

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from 'src/app/model/task';
+import { CrudServiceService } from 'src/app/service/crud-service.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +8,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+taskObj : Task = new Task();
+taskArray : Task[] = [];
 
-  constructor() { }
+addTaskValue : string = '';
+editTaskValue : string = '';
+
+  constructor(private crudService : CrudServiceService) { }
 
   ngOnInit(): void {
+    this.addTaskValue = '';
+    this.editTaskValue = '';
+    this.taskObj = new Task();
+    this.taskArray = [];
+    this.getAllTask();
   }
 
+  addTask(){
+    this.taskObj.task_name = this.addTaskValue;
+    this.crudService.addTask(this.taskObj).subscribe( res => {
+      this.ngOnInit();
+      this.addTaskValue = '';
+    }, err => {
+      alert(err);
+    });
+  }
+
+  getAllert(){
+    alert("okay");
+  }
+
+  getAllTask(){
+    this.crudService.getAllTask().subscribe( res => {
+      this.taskArray = res;
+    }, err => {
+      alert(err);
+    });
+  }
+
+  editTask(){
+    this.taskObj.task_name = this.editTaskValue;
+    this.crudService.editTask(this.taskObj).subscribe( res => {
+      this.ngOnInit();
+    }, err => {
+      alert("Task update failed");
+    });
+  }
+
+  deleteTask(task : Task){
+    this.crudService.deleteTask(task).subscribe( res => {
+      this.ngOnInit();
+    }, res => {
+      alert("Task delete failed");
+    });
+  }
+
+  call(task : Task){
+    this.taskObj = task;
+    this.editTaskValue = task.task_name;
+  }
 }
